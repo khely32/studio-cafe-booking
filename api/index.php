58a29697d973e1045f778c04ff2805f5aeec1ae5
khely32 <perezck40@gmail.com>
2026-07-31 11:53:17 +0800

    fix: robust env injection, surface fatals, raise function maxDuration to 60s

--- api/index.php
+++ api/index.php
@@ -12,18 +12,40 @@
 |
 */
 
-putenv('APP_NAME=56\'30 Studio Cafe');
-putenv('APP_ENV=production');
-putenv('APP_DEBUG=true');
-putenv('APP_CONFIG_CACHE=/tmp/config.php');
-putenv('APP_EVENTS_CACHE=/tmp/events.php');
-putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
-putenv('APP_ROUTES_CACHE=/tmp/routes.php');
-putenv('APP_SERVICES_CACHE=/tmp/services.php');
-putenv('CACHE_DRIVER=array');
-putenv('CACHE_STORE=array');
-putenv('LOG_CHANNEL=stderr');
-putenv('SESSION_DRIVER=cookie');
-putenv('VIEW_COMPILED_PATH=/tmp');
+$_ENV['APP_NAME'] = "56'30 Studio Cafe";
+$_SERVER['APP_NAME'] = "56'30 Studio Cafe";
+$_ENV['APP_ENV'] = 'production';
+$_SERVER['APP_ENV'] = 'production';
+$_ENV['APP_DEBUG'] = 'true';
+$_SERVER['APP_DEBUG'] = 'true';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_SERVER['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
+$_SERVER['APP_EVENTS_CACHE'] = '/tmp/events.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_SERVER['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_SERVER['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_SERVER['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['CACHE_DRIVER'] = 'array';
+$_SERVER['CACHE_DRIVER'] = 'array';
+$_ENV['CACHE_STORE'] = 'array';
+$_SERVER['CACHE_STORE'] = 'array';
+$_ENV['LOG_CHANNEL'] = 'stderr';
+$_SERVER['LOG_CHANNEL'] = 'stderr';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+$_SERVER['SESSION_DRIVER'] = 'cookie';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+$_SERVER['VIEW_COMPILED_PATH'] = '/tmp';
+
+register_shutdown_function(function () {
+    $error = error_get_last();
+    if ($error && ($error['type'] & (E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR))) {
+        http_response_code(500);
+        header('Content-Type: text/plain');
+        echo "FATAL: {$error['message']}\n{$error['file']}:{$error['line']}\n";
+    }
+});
 
 require __DIR__ . '/../public/index.php';
